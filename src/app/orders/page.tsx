2083a2ff77cde7f9a0747708570bf1b1b093aabf
quanphung0106-sudo <phungdinhquan16@gmail.com>
2023-08-21 16:33:57 +0700

    React-query mutation

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,44 +1,113 @@
-import React from "react";
+"use client";
+
+import { OrderType } from "@/types/types";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { stringify } from "querystring";
+import { FormEvent } from "react";
 
 const OrdersPage = () => {
-  return (
-    <div className="p-4 lg:px-20 xl:px-40">
-      <table className="w-full border-separate border-spacing-3">
-        <thead>
-          <tr className="text-left">
-            <th className="hidden md:block">Order ID</th>
-            <th>Date</th>
-            <th>Price</th>
-            <th className="hidden md:block">Products</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="text-sm md:text-base bg-red-50">
-            <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
-            <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
-            <td className="py-6 px-1">On the way (approx. 10min)...</td>
-          </tr>
-          <tr className="text-sm md:text-base odd:bg-gray-100">
-            <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
-            <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
-            <td className="py-6 px-1">On the way (approx. 10min)...</td>
-          </tr>
-          <tr className="text-sm md:text-base odd:bg-gray-100">
-            <td className="hidden md:block py-6 px-1">1237861238721</td>
-            <td className="py-6 px-1">19.07.2023</td>
-            <td className="py-6 px-1">89.90</td>
-            <td className="hidden md:block py-6 px-1">Big Burger Menu (2), Veggie Pizza (2), Coca Cola 1L (2)</td>
-            <td className="py-6 px-1">On the way (approx. 10min)...</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    if (status === "unauthenticated") {
+        router.push("/");
+    }
+    const { isLoading, error, data } = useQuery({
+        queryKey: ["orders"],
+        queryFn: () =>
+            fetch("http://localhost:3000/api/orders").then((res) => res.json()),
+    });
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: ({ id, status }: { id: string; status: string }) => {
+            return fetch(`http://localhost:3000/api/orders/${id}`, {
+              method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(status),
+            });
+        },
+        onSuccess() {
+            queryClient.invalidateQueries({ queryKey: ["orders"] });
+        },
+    });
+
+    if (isLoading || status === "loading") return "Loading...";
+
+    const handleUpdate = (e: FormEvent<HTMLFormElement>, id: string) => {
+        e.preventDefault();
+        const form = e.target as HTMLFormElement;
+        const input = form.elements[0] as HTMLInputElement;
+        const status = input.value;
+
+        mutation.mutate({ id, status });
+    };
+
+    return (
+        <div className="p-4 lg:px-20 xl:px-40">
+            <table className="w-full border-separate border-spacing-3">
+                <thead>
+                    <tr className="text-left">
+                        <th className="hidden md:block">Order ID</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th className="hidden md:block">Products</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item: OrderType) => (
+                        <tr
+                            key={item.id}
+                            className="text-sm md:text-base bg-red-50"
+                        >
+                            <td className="hidden md:block py-6 px-1">
+                                {item.id}
+                            </td>
+                            <td className="py-6 px-1">
+                                {item.createdAt.toString().slice(0, 10)}
+                            </td>
+                            <td className="py-6 px-1">{item.price}</td>
+                            <td className="hidden md:block py-6 px-1">
+                                {item.products[0].title}
+                            </td>
+                            {session?.user.isAdmin ? (
+                                <td>
+                                    <form
+                                        className="flex items-center justify-center gap-4"
+                                        onSubmit={(e) =>
+                                            handleUpdate(e, item.id)
+                                        }
+                                    >
+                                        <input
+                                            placeholder={item.status}
+                                            className="p-2 ring-1 ring-red-100"
+                                        />
+                                        <button className="bg-red-400 p-2 rounded-full">
+                                            <Image
+                                                src="/edit.png"
+                                                alt="Delete"
+                                                width={20}
+                                                height={20}
+                                            />
+                                        </button>
+                                    </form>
+                                </td>
+                            ) : (
+                                <td className="py-6 px-1">{item.status}</td>
+                            )}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default OrdersPage;
